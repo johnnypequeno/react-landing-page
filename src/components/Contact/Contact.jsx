e48ad27b015f26542628f3e2d077d1527cabd5b3
johnnypequeno <johnnypequeno7@gmail.com>
2023-04-21 19:57:27 -0300

    Modified path of background image in App.css

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -36,7 +36,8 @@ function Contact() {
     return (
 
     <form onSubmit= {enviarFormulario}>
-        
+        <>
+        <div id="contact"> </div> 
         <h2>Entre em contato</h2>
         <fieldset>
             <label htmlFor="input-name">Nome</label>
@@ -94,22 +95,9 @@ function Contact() {
         <center>
             <input type="submit" value="Enviar" className="button"/>
         </center>
-
+        </>   
     </form>
-    /*<>
-        <div id="contact" className="area-form">
-            <h2>Entre em contato</h2>
-            <div className="form-box"> 
-                <form>
-                    <input type="text" id="f-name" placeholder="Nome" required></input>
-                    <input type="email" id="f-email" placeholder="Email" required></input>
-                    <input type="tel" id="f-phone" placeholder="Telefone"></input>
-                    <textarea name="message" id="f-message" cols="30" rows="7" placeholder="Deixe sua mensagem"></textarea>
-                    <input className="btn-submit" type="submit" id="f-button" value="Enviar"></input>
-                </form>
-            </div>
-        </div>
-    </>*/
+   
     )
 }
 
