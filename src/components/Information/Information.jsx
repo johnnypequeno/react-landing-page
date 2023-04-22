afb6a1ae0894a70d397dc2d6cf9cb3564bc1f9f4
johnnypequeno <johnnypequeno7@gmail.com>
2023-04-22 17:15:47 -0300

    test 3

--- src/components/Information/Information.jsx
+++ src/components/Information/Information.jsx
@@ -13,7 +13,7 @@ function Information() {
          <aside>
              <h2>Olá, vamos produzir juntos?</h2>
              <p> Sou jornalista e produtor audiovisual, com mais de 10 anos de carreira. Aqui, você vai conhecer o meu trabalho e descobrir como posso ajudar a produzir histórias de sucesso para sua marca.</p>
-             <a href="/curriculum" idName="saiba-btn"><li>Saiba mais</li></a>
+             <a href="/curriculum"><li>Saiba mais</li></a>
         </aside>
     </main> 
     <Portfolio></Portfolio>
