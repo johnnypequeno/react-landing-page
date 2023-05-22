425388f4b07576184c5227a33f42bde36480c47d
johnnypequeno <johnnypequeno7@gmail.com>
2023-05-22 02:10:21 -0300

    updated api js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create  ({
-    baseURL: 'http://localhost:5000'
+    baseURL: 'https://json-server-node-qaj2.onrender.com'
 });
 
 export default api
